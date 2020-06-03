5b7df1d6d7ec7bab93d02dbae18824ae0459152d
Inkant Awasthi <10158214+tim1016@users.noreply.github.com>
2020-06-02 23:36:44 -0500

    renamed the schematics folder and package name

---
+++ src/resume/index.ts
@@ -0,0 +1,48 @@
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  url,
+  apply,
+  template,
+  mergeWith,
+  SchematicsException,
+  move,
+} from '@angular-devkit/schematics';
+
+import { buildDefaultPath } from '@schematics/angular/utility/project';
+import { parseName } from '@schematics/angular/utility/parse-name';
+import { Schema } from './schema';
+import { strings } from '@angular-devkit/core';
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function hello(_options: Schema): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    const workspaceConfigBuffer = tree.read('angular.json');
+    if (!workspaceConfigBuffer) {
+      throw new SchematicsException('angular.json cannot be found');
+    }
+    const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
+    const projectName = _options.project || workspaceConfig.defaultProject;
+    const project = workspaceConfig.projects[projectName];
+
+    const defaultProjectPath = buildDefaultPath(project);
+    const parsedPath = parseName(defaultProjectPath, _options.name);
+
+    const { name, path } = parsedPath;
+
+    const sourceTemplates = url('./files');
+    const sourceParametrizedTemplates = apply(sourceTemplates, [
+      template({
+        ..._options,
+        ...strings,
+        name,
+      }),
+      move(path),
+    ]);
+    // const { name } = _options;
+    // tree.create('hello.js', `console.log('Hello ${name}')`);
+    return mergeWith(sourceParametrizedTemplates)(tree, _context);
+  };
+}
